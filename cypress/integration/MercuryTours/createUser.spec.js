b4d285b0b5924cd2b23cb7fe77b88eb51c82b1b8
fyznitch <fyznitch@gmail.com>
2020-04-26 22:07:13 -0700

    Added book flight

--- cypress/integration/MercuryTours/createUser.spec.js
+++ cypress/integration/MercuryTours/createUser.spec.js
@@ -40,4 +40,17 @@ describe('Login to the application after creating account', () => {
             .should('be.visible')
         
     });
+
+    it('Should book a flight', () => {
+        cy.get('select[name="passCount"]').select('4');
+        cy.get('select[name="fromPort"]').select('London');
+        cy.get('select[name="fromMonth"]').select('May');   
+        cy.get('select[name="fromDay"]').select('1');
+        
+        cy.get('select[name="toPort"]').select('Portland'); 
+        cy.get('select[name="toMonth"]').select('June');
+        cy.get('select[name="toDay"]').select('15');   
+        cy.get('input[name="findFlights"]').click();      
+        
+    });
 });
